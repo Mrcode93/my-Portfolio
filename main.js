aad0a061d7d210cb45cb78bd7c45e9d3abfffcd9
Mrcode93 <alhlaqalhlaq93@gmail.com>
2022-06-12 03:30:03 +0300

    v 2.1

--- main.js
+++ main.js
@@ -1,21 +1,116 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
-
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
-        }s`;
+let ul = document.querySelector("ul");
+let navLink = document.querySelectorAll("ul li");
+let loader = document.querySelector(".loader");
+//set active class to list of links
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].onclick = function() {
+        navLink[i].className = "active";
+        for (let j = 0; j < navLink.length; j++) {
+            if (j !== i) {
+                navLink[j].className = "";
             }
-        });
+        }
+    };
+}
 
-        burger.classList.toggle("toggle");
+//landing page anime
+let h1 = document.querySelector(".landing h1");
+document.addEventListener("DOMContentLoaded", () => {
+    loader.style.display = "none";
+    anime({
+        targets: "ul li",
+        translateX: [
+            { value: -500, duration: 0 },
+            { value: 0, duration: 50 },
+        ],
+        delay: anime.stagger(100, { start: 500 }), // delay starts at 500ms then increase by 100ms for each elements.
     });
-};
-navSlide();
+    //set anime for logo
+    anime({
+        targets: ".logo img",
+        translateY: [
+            { value: -100, duration: 0 },
+            { value: 0, duration: 1000 },
+        ],
+        rotate: [
+            { value: 0, duration: 0 },
+            { value: 360, duration: 1000 },
+        ],
+        delay: anime.stagger(100, { start: 500 }),
+    });
+    // set anime for h1
+    anime({
+        targets: ".landing h1",
+        scale: [
+            { value: 0, duration: 0 },
+            { value: 10, duration: 0 },
+            { value: 1, duration: 2000 },
+        ],
+        delay: anime.stagger(100, { start: 500 }),
+    });
+    //set fly letters for p
+    anime({
+        targets: ".landing p",
+        translateX: [{
+                value: -1000,
+                duration: 0,
+            },
+            {
+                value: 0,
+                duration: 1000,
+            },
+        ],
+        delay: anime.stagger(100, { start: 500 }),
+    });
+    //set fall anime for buttons
+    anime({
+        targets: ".buttons",
+        translateY: [{
+                value: -2000,
+                duration: 0,
+            },
+            {
+                value: 0,
+                duration: 1000,
+            },
+        ],
+    });
+    anime({
+        targets: ".image-holder img",
+        translateX: [{
+                value: -2000,
+                duration: 0,
+            },
+            {
+                value: 0,
+                duration: 1000,
+            },
+        ],
+        rotateZ: [{
+                value: 360,
+                duration: 0,
+            },
+            {
+                value: 0,
+                duration: 1000,
+            },
+        ],
+    });
+});
+// set anime for loader
+
+document.addEventListener("load", () => {});
+//
+// let toTop = document.querySelector(".top");
+// window.onscroll = () => {
+// if (window.scrollY > 900) {
+//     toTop.style.display = "flex";
+// } else {
+//     toTop.style.display = "none";
+// }
+// };
+// toTop.addEventListener("click", () => {
+//     window.scrollTo({
+//         top: 0,
+//     });
+// });

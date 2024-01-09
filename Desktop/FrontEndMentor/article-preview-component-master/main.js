c57a59ab8d2b3048991a04906b06ee3693953747
Mrcode93 <89423412+Mrcode93@users.noreply.github.com>
2024-01-09 22:45:52 +0300

    Delete Desktop directory

--- Desktop/FrontEndMentor/article-preview-component-master/main.js
+++
@@ -1,6 +0,0 @@
-let btn = document.querySelector(".bottom button");
-let share = document.querySelector(".share");
-btn.addEventListener("click", () => {
-    share.classList.toggle("active");
-    btn.classList.toggle("active");
-});
